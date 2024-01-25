282854a384a372de918ff190e0e8369f790fb1ab
Yevhen Paschenko <ypasch@softserveinc.com>
2024-01-25 15:26:42 +0200

    wip

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,16 +1,21 @@
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header';
+import CoreConcept from './components/CoreConcepts/CoreConcept';
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept concept = {CORE_CONCEPTS[0]}/>
+            <CoreConcept concept = {CORE_CONCEPTS[1]}/>
+            <CoreConcept concept = {CORE_CONCEPTS[2]}/>
+            <CoreConcept concept = {CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
